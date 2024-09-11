1d82eb5c6a24060ddc2c09098602ac96b21198ea
chris <landwaterfood@gmail.com>
2024-09-11 18:28:54 +0100

    commit the step at shuffle

--- Blackjack2.php
+++ Blackjack2.php
@@ -1,9 +1,25 @@
 <?php
 $suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 $cardvalues = ["2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10, "A"=>11 ];
-$deck = [];
 
-foreach ($suit as $suits);
+$deck = [];
+foreach ($suits as $suit) {
+    foreach ($cardvalues as $key => $value) {
+        $deck[] = ["suit" => $suit, "value" => $key, "points" => $value];
+    }
+}
+shuffle($deck);
+
+$player_1_hand = [];
+while()
+//$player_2_hand = [];
+
+
+
+
+
+
+
 
 
 
