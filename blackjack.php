17602c2c7c36371c1bb100a34a4a83fe8a034c09
chris <landwaterfood@gmail.com>
2024-09-11 16:23:25 +0100

    commiting to change the the repo

---
+++ blackjack.php
@@ -0,0 +1,78 @@
+<?php
+        $deck=[$suit1 = [
+            "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10, "A"=>11 ], $suit2 = [
+            "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10, "A"=>11 ], $suit3 = [
+            "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10, "A"=>11 ], $suit4 = [
+            "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10, "A"=>11 ]];
+
+
+
+
+        shuffle($deck);
+        var_dump($deck);
+    function drawcard($player){
+
+    }
+
+     foreach ($deck as $key => $value){
+            echo $key."==>".$value."\n";
+
+            function drawcard($player){
+            global $deck, $deck;
+            return end($deck);
+
+        }
+
+
+       // $num = ; $num2 = ;
+       // $player_1_hand = [foreach (drawcard($player_1_hand))]{
+
+}
+;
+       // $player_1_hand = drawcard($player_1_hand);
+
+      //  $player_1_hand = [];
+      //  $player_1_hand = drawcard($player_1_hand);
+
+
+      //  array_pop($deck);
+
+     //   $play1score= foreach($player_1_hand, )
+
+
+
+       //     function scoretoal($num, $num1) : int
+            {
+        //        return $num + $num1;
+         //   }
+
+
+
+
+
+ //If ($player1score or $player2score > 21) {
+ //    echo "Draw";
+ //    } elseif ($player1score >)
+ }
+
+//	$coat_message = 'Yes, it is raining';
+//} else if {
+//	$coat_message = 'No, it is not raining';
+
+
+
+
+
+       // $player_2_hand[] = [drawcard()];
+
+
+
+
+/*
+I need to pull four random key values from the deck and put them into 2 separate player arrays
+add the values of ech player,
+the cards are removed from the deck
+   if
+*\
+ */
+
